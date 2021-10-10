24816701e1e77f68c5349317c1ad03bd7c74cf05
Manuel de la Torre <manuelmhtr@gmail.com>
2021-10-09 20:13:21 -0500

    Assignment1, trie

---
+++ README.md
@@ -0,0 +1,13 @@
+# Algorithms on strings
+
+My assignments for the UC San Diego [Algorithms on strings](https://www.coursera.org/learn/algorithms-on-strings) course.
+
+## Assignment 1
+
+### Trie
+
+Builds prefix tries given a list of patterns
+
+```sh
+$ cat <file with input> | python3 ./assignment1/trie/trie.py
+```
---
+++ assignment1/trie/trie.py
@@ -0,0 +1,47 @@
+#Uses python3
+import sys
+
+# Return the trie built from patterns
+# in the form of a dictionary of dictionaries,
+# e.g. {0:{'A':1,'T':2},1:{'C':3}}
+# where the key of the external dictionary is
+# the node ID (integer), and the internal dictionary
+# contains all the trie edges outgoing from the corresponding
+# node, and the keys are the letters on those edges, and the
+# values are the node IDs to which these edges lead.
+
+class Trie:
+    def __init__(self):
+        self.tree = {}
+        self.nodes_count = 0
+
+    def add_patterns(self, patterns):
+        for pattern in patterns:
+            self.add_pattern(pattern, 0)
+
+    def add_pattern(self, pattern, from_node):
+        if not pattern:
+            return
+        char = pattern[0]
+        to_node = self.tree.get(from_node, {}).get(char)
+        if not to_node:
+            self.nodes_count += 1
+            to_node = self.nodes_count
+            self.add_edge(from_node, to_node, char)
+        self.add_pattern(pattern[1:], to_node)
+
+    def add_edge(self, from_node, to_node, label):
+        self.tree[from_node] = self.tree.get(from_node, {})
+        self.tree[from_node][label] = to_node
+
+def build_trie(patterns):
+    trie = Trie()
+    trie.add_patterns(patterns)
+    return trie.tree
+
+if __name__ == '__main__':
+    patterns = sys.stdin.read().split()[1:]
+    tree = build_trie(patterns)
+    for node in tree:
+        for c in tree[node]:
+            print("{}->{}:{}".format(node, tree[node][c], c))
